f1319028d001307029ede9456aa790981aa8b080
kassembler <mirjam.kristi@gmail.com>
2025-10-19 16:38:21 +0300

    Kood

---
+++ skript.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById("Img").style.border = "2px solid #828282";
+    document.getElementById("Img").style.boxShadow = "0 10px 4px 0 rgba(0, 0, 0, 0.048)";
+}
